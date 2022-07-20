9e5b814fc1c313458c51d4c3d084001320aaddc4
suchan <aud248@naver.com>
2022-07-20 14:51:09 +0900

    test findAll

--- src/main/java/com/io/linkapp/member/repository/MemberRepository.java
+++ src/main/java/com/io/linkapp/member/repository/MemberRepository.java
@@ -1,7 +1,7 @@
 package com.io.linkapp.member.repository;
 
 import com.io.linkapp.member.domain.Member;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends CrudRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
