dc6040564ad22895058926bbc025966eae739df8
suchan <aud248@naver.com>
2022-07-20 10:04:48 +0900

    test RDS Connection

--- src/main/java/com/io/linkapp/member/service/MemberService.java
+++ src/main/java/com/io/linkapp/member/service/MemberService.java
@@ -1,4 +1,21 @@
 package com.io.linkapp.member.service;
 
+import com.io.linkapp.member.domain.Member;
+import com.io.linkapp.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class MemberService {
+        private final MemberRepository memberRepository;
+
+        public Member findById(Long id){
+            return memberRepository.findById(id).get();
+        }
+
+        public void save(){
+            Member member = new Member();
+            memberRepository.save(member);
+        }
 }
