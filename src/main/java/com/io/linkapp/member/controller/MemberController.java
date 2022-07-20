5bfa1d1c4e27bb30ad5aee1cb6de51573aa82509
suchan <aud248@naver.com>
2022-07-20 10:50:57 +0900

    chore jpa option

--- src/main/java/com/io/linkapp/member/controller/MemberController.java
+++ src/main/java/com/io/linkapp/member/controller/MemberController.java
@@ -3,7 +3,6 @@ package com.io.linkapp.member.controller;
 import com.io.linkapp.member.domain.Member;
 import com.io.linkapp.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
