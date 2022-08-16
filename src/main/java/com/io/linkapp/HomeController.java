49a80b9bb933c59d9c6e6be096480e2386eaeacd
suchan <aud248@naver.com>
2022-08-16 17:25:10 +0900

    Feat: Add Redis Server
    * 레디스 서버 추가
    * EC2 내부 Docker Container 구동

--- src/main/java/com/io/linkapp/HomeController.java
+++ src/main/java/com/io/linkapp/HomeController.java
@@ -1,6 +1,8 @@
 package com.io.linkapp;
 
 import com.io.linkapp.config.security.auth.PrincipalDetails;
+import com.io.linkapp.user.service.RedisService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final RedisService redisService;
 
     @ResponseBody
     @GetMapping("/")
@@ -23,7 +28,15 @@ public class HomeController {
     }
 
     @GetMapping("/swagger")
-    public String swagger(){
+    public String swagger() {
         return "redirect:/swagger-ui.html";
+    }
+
+    @ResponseBody
+    @GetMapping("/redis")
+    public String redis(@AuthenticationPrincipal PrincipalDetails principalDetails){
+        String username = principalDetails.getUser().getUsername();
+        String refreshToken = redisService.getValues(username);
+        return refreshToken;
     }
 }
