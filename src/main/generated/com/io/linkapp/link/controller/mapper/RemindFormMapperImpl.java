fadeb87a3003bec1edd3d325583d5af981b739b6
신정아 <jeonga@khu.ac.kr>
2022-08-17 21:38:18 +0900

    fix
    컴파일 관련 에러 수정

---
+++ src/main/generated/com/io/linkapp/link/controller/mapper/RemindFormMapperImpl.java
@@ -0,0 +1,59 @@
+package com.io.linkapp.link.controller.mapper;
+
+import com.io.linkapp.link.domain.Remind;
+import com.io.linkapp.link.request.RemindRequest.Add;
+import com.io.linkapp.link.response.RemindResponse.GetAll;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-08-17T20:34:35+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+@Component
+public class RemindFormMapperImpl extends RemindFormMapper {
+
+    @Override
+    public Remind toRemind(Add in) {
+        if ( in == null ) {
+            return null;
+        }
+
+        Remind remind = new Remind();
+
+        remind.setRemindTitle( in.getRemindTitle() );
+
+        return remind;
+    }
+
+    @Override
+    public GetAll toGetAll(Remind in) {
+        if ( in == null ) {
+            return null;
+        }
+
+        GetAll getAll = new GetAll();
+
+        getAll.setRemindId( in.getRemindId() );
+        getAll.setRemindTitle( in.getRemindTitle() );
+
+        return getAll;
+    }
+
+    @Override
+    public List<GetAll> toGetAllList(List<Remind> in) {
+        if ( in == null ) {
+            return null;
+        }
+
+        List<GetAll> list = new ArrayList<GetAll>( in.size() );
+        for ( Remind remind : in ) {
+            list.add( toGetAll( remind ) );
+        }
+
+        return list;
+    }
+}
