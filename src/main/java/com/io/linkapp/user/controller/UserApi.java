b60ec4f6c5ee6efa3d0df4e2c75e5d133c078346
suchanmyoung <aud248@naver.com>
2022-08-21 23:36:19 +0900

    Feat: Add kakao login in native app

--- src/main/java/com/io/linkapp/user/controller/UserApi.java
+++ src/main/java/com/io/linkapp/user/controller/UserApi.java
@@ -1,16 +1,22 @@
 package com.io.linkapp.user.controller;
 
+import com.io.linkapp.config.security.jwt.JwtResponse;
+import com.io.linkapp.link.request.KakaoRequest;
 import com.io.linkapp.user.domain.User;
 import com.io.linkapp.user.request.UserRequest;
 import com.io.linkapp.user.response.UserResponse;
+import com.io.linkapp.user.service.RedisService;
 import com.io.linkapp.user.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Api(value = "User", tags = {"User"})
 @RequiredArgsConstructor
@@ -18,6 +24,7 @@ import java.util.List;
 public class UserApi {
 
     private final UserService userService;
+    private final RedisService redisService;
 
     @ApiOperation("회원 가입")
     @PostMapping("/user")
@@ -38,8 +45,9 @@ public class UserApi {
     }
 
     @ApiOperation(value = "로그인", notes = "로그인 시 실제 요청은 Spring Security 가 수행함. 클라이언트 확인을 위한 API")
-    @PostMapping("/login")
-    public void loginDummyApi(@RequestBody UserRequest userRequest){
+    @PostMapping("/kakao")
+    public JwtResponse kakaoLogin(@RequestBody KakaoRequest kakaoRequest) {
+        return userService.kakaoLogin(kakaoRequest);
     }
 
     @ApiOperation(value = "카카오 로그인 주소", notes = "동작하지 않음. 요청 주소 확인을 위한 API")
