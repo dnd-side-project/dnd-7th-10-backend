0a5e0c423364149e225c8b108e704f453bf6a3e9
suchan <aud248@naver.com>
2022-08-23 15:34:33 +0900

    Test : Test ArticleApi

--- src/test/java/com/io/linkapp/link/controller/api/ArticleApiTest.java
+++ src/test/java/com/io/linkapp/link/controller/api/ArticleApiTest.java
@@ -1,130 +1,139 @@
-//package com.io.linkapp.link.controller.api;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.io.linkapp.exception.ArticleNotFoundException;
-//import com.io.linkapp.link.domain.Article;
-//import com.io.linkapp.link.domain.Memo;
-//import com.io.linkapp.link.mapper.ArticleMapper;
-//import com.io.linkapp.link.repository.ArticleRepository;
-//import com.io.linkapp.link.repository.MemoRepository;
-//import com.io.linkapp.link.request.ArticleRequest;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import java.util.UUID;
-//
-//import static org.springframework.http.MediaType.APPLICATION_JSON;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@AutoConfigureMockMvc
-//@SpringBootTest
-//class ArticleApiTest {
-//
-//    @Autowired
-//    MockMvc mockMvc;
-//
-//    @Autowired
-//    ObjectMapper objectMapper;
-//
-//    @Autowired
-//    ArticleRepository articleRepository;
-//
-//    @Autowired
-//    MemoRepository memoRepository;
-//
-//    private static ArticleRequest request;
-//
-//    @BeforeEach
-//    void articleSetup(){
-//        UUID uuid = UUID.randomUUID();
-//
-//       request = ArticleRequest.builder()
-//                .folderId(uuid)
-//                .remindId(uuid)
-//                .userId(uuid)
-//                .linkContent("getTest")
-//                .linkTitle("getTest")
-//                .build();
-//    }
-//
-//    @Test
-//    @DisplayName("POST: /article 요청 시 링크가 저장된다")
-//    void addTest() throws Exception {
-//        //expected
-//        mockMvc.perform(post("/article")
-//                        .contentType(APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(request)))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("GET: /article/{id}로 링크를 조회한다")
-//    void getLink() throws Exception{
-//        Article article = ArticleMapper.INSTANCE.toEntity(request);
-//
-//        articleRepository.save(article);
-//
-//        //expected
-//        mockMvc.perform(get("/article/{id}", article.getId())
-//                        .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").value(article.getId().toString()))
-//                .andExpect(jsonPath("$.linkContent").value("getTest"))
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("아티클 조회 시 아티클에 등록된 메모 모두 불러온다")
-//    void getArticleMemoTest() throws Exception {
-//        //given
-//        Article article = ArticleMapper.INSTANCE.toEntity(request);
-//
-//        articleRepository.save(article);
-//
-//        Memo memo = Memo.builder()
-//                .article(article)
-//                .content("아티클에 등록된 메모 첫 번째")
-//                .build();
-//
-//        Memo memo2 = Memo.builder()
-//                .article(article)
-//                .content("아티클에 등록된 메모 두 번째")
-//                .build();
-//
-//        memoRepository.save(memo);
-//        memoRepository.save(memo2);
-//
-//        //when
-//        mockMvc.perform(get("/article/{id}", article.getId())
-//                        .contentType(APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @DisplayName("DELETE: /article/{id} 요청 시 링크를 삭제한다")
-//    void removeArticleTest() throws Exception {
-//        //given
-//        Article article = articleRepository.save(ArticleMapper.INSTANCE.toEntity(request));
-//
-//        //when
-//        mockMvc.perform(delete("/article/{id}", article.getId()))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//
-//        //then
-//        Assertions.assertThrows(ArticleNotFoundException.class,
-//                () -> articleRepository.findById(article.getId())
-//                        .orElseThrow(ArticleNotFoundException::new));
-//    }
-//}
+package com.io.linkapp.link.controller.api;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.io.linkapp.config.security.jwt.JwtProperty;
+import com.io.linkapp.exception.CustomGlobalException;
+import com.io.linkapp.exception.ErrorCode;
+import com.io.linkapp.link.domain.Article;
+import com.io.linkapp.link.domain.Folder;
+import com.io.linkapp.link.domain.Memo;
+import com.io.linkapp.link.mapper.ArticleMapper;
+import com.io.linkapp.link.repository.ArticleRepository;
+import com.io.linkapp.link.repository.FolderRepository;
+import com.io.linkapp.link.repository.MemoRepository;
+import com.io.linkapp.link.request.ArticleRequest;
+import com.io.linkapp.user.domain.User;
+import com.io.linkapp.user.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Transactional
+class ArticleApiTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    private static ArticleRequest request;
+    private static String jwtToken;
+
+    @BeforeEach
+    void setUp() {
+        User user = User.builder()
+                .username("testUser")
+                .password("test")
+                .build();
+
+        user = userRepository.save(user);
+
+        Folder folder = Folder.builder()
+                .user(user)
+                .folderTitle("testFolder")
+                .build();
+
+        folder = folderRepository.save(folder);
+
+        request = ArticleRequest.builder()
+                .folderId(folder.getFolderId())
+                .linkUrl("www.naver.com")
+                .build();
+
+        jwtToken = JWT.create()
+                .withSubject(JwtProperty.SUBJECT)
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperty.EXPIRATION_TIME))
+                .withClaim("username", "chan")
+                .sign(Algorithm.HMAC512(JwtProperty.SECRET));
+    }
+
+    @Test
+    @DisplayName("POST: /article 요청 시 링크가 저장된다")
+    void addTest() throws Exception {
+        //expected
+        mockMvc.perform(post("/article")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                        .header(JwtProperty.HEADER, JwtProperty.TOKEN_PREFIX + jwtToken))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.linkUrl").value("www.naver.com"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("GET: /article/{id}로 링크를 조회한다")
+    void getLink() throws Exception {
+        Article article = ArticleMapper.INSTANCE.toEntity(request);
+
+        article = articleRepository.save(article);
+
+        //expected
+        mockMvc.perform(get("/article/{articleId}", article.getId())
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(article.getId().toString()))
+                .andExpect(jsonPath("$.linkUrl").value("www.naver.com"))
+                .andDo(print());
+    }
+
+
+    @Test
+    @DisplayName("DELETE: /article/{id} 요청 시 링크를 삭제한다")
+    void removeArticleTest() throws Exception {
+        //given
+        Article article = articleRepository.save(ArticleMapper.INSTANCE.toEntity(request));
+
+        //when
+        mockMvc.perform(delete("/article/{id}", article.getId()))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        //then
+        assertThrows(CustomGlobalException.class,
+                () -> articleRepository.findById(article.getId())
+                        .orElseThrow(() -> new CustomGlobalException(ErrorCode.ARTICLE_NOT_FOUND)));
+    }
+}
