7d54373fd2151675ed79ffb36642a3948be04448
suchan <aud248@naver.com>
2022-08-29 10:48:08 +0900

    Fix: Add message, status field in GlobalException
    * 기존 예외 발생 시 message 미노출로 로그로 파악하기 어렵고 테스트가 불가능 하여 필드 추가

--- src/main/java/com/io/linkapp/exception/CustomGlobalException.java
+++ src/main/java/com/io/linkapp/exception/CustomGlobalException.java
@@ -1,13 +1,18 @@
 package com.io.linkapp.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomGlobalException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private final String message;
+    private final HttpStatus status;
     
     public CustomGlobalException(ErrorCode errorCode){
         this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.status = errorCode.getStatus();
     }
 }
