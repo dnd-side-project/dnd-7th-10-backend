ec2f63bbb91184ee26a8dfa5cec3e63e7fe01bbd
suchan <aud248@naver.com>
2022-08-30 10:48:50 +0900

    Refactor: Refactor quartz logging
    * 쿼츠 로깅을 System.out 에서 logger 로 변경

--- src/main/java/com/io/linkapp/link/controller/api/SchedulerApi.java
+++ src/main/java/com/io/linkapp/link/controller/api/SchedulerApi.java
@@ -2,17 +2,13 @@ package com.io.linkapp.link.controller.api;
 
 import com.io.linkapp.config.quartz.QuartzService;
 import com.io.linkapp.config.security.auth.PrincipalDetails;
-import com.io.linkapp.link.controller.predicate.TagFormPredicate;
 import com.io.linkapp.link.request.PushRequest;
-import com.io.linkapp.link.request.TagRequest;
-import com.io.linkapp.link.response.TagResponse.GetAll;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +25,7 @@ public class SchedulerApi {
     @ApiOperation("알림 스케줄러 설정")
     @PostMapping("/quartz")
     public void testQuartzForReal(@RequestBody PushRequest pushRequest,@AuthenticationPrincipal PrincipalDetails principalDetails){ //여기서 사용자 정보 Auth객체로 받고
-        System.out.println("quartzForReal Api");
-        service.init2(pushRequest, principalDetails.getUser()); //여기로 넘겨준 다음에
+        service.initSchedule(pushRequest, principalDetails.getUser()); //여기로 넘겨준 다음에
     }
     
     
